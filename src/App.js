2e9432e674491a9786cbcd08ca641e5b1392ade7
eddgalan <edd.galan@hotmail.com>
2022-05-26 22:21:42 -0500

    Create first component - Creación del primer componente usando props

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ function App() {
   return (
     <div className="app">
       <div className="text-center">
-        <h1 className="text-center"> Comments </h1>
+        <h1 className="text-center mt-5"> Comments </h1>
         <div className="p-5">
-          <Comment/>
+          <Comment name="Edson Galan" position="Fullstack Developer" company="Alpha Team" img="user-00.png"
+            comment="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+          <Comment name="Juan Pérez" position="Backend Developer" company="Amazon" img="user-00.png"
+            comment="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+          <Comment name="Luis García" position="Frontend Developer" company="Uber" img="user-00.png"
+            comment="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
         </div>
       </div>
     </div>
--- src/components/comments/comments.jsx
+++ src/components/comments/comments.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from 'react'; // Esta línea es opcional
 import './comments.css';
 
-function Comment() {
+function Comment(props) {
   return (
-    <div class="card">
-      <div class="card-body p-0">
+    <div className="card mb-3">
+      <div className="card-body p-0">
         <div className="row">
           <div className="col-md-5">
-            <img className="img-fluid comment-img" src={ require('../../img/user-00.png') } alt='Foto User' />
+            <img className="img-fluid comment-img" src={ require(`../../img/${ props.img }`) } alt={ props.name } />
           </div>
           <div className="col-md-7 text-content">
-            <p class="comment-user-name"> Edson Galan </p>
-            <p class="comment-position"> Fullstrack Developer </p>
-            <p class="comment">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p className="comment-user-name"> { props.name } </p>
+            <p className="comment-position"> { props.position } in <strong> { props.company } </strong> </p>
+            <p className="comment"> "{ props.comment }" </p>
           </div>
         </div>
       </div>
