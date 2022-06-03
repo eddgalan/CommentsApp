d387e123a67b58d1cc31682710c6bb3a1f11283a
eddgalan <edd.galan@hotmail.com>
2022-06-02 19:23:37 -0500

    Convert to class-component - Se adaptaron los componentes funcionales a componentes de clase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './bootstrap/bootstrap.min.css';
 import Comment from './components/comments/comments';
 
-function App() {
-  return (
-    <div className="app">
-      <div className="text-center">
-        <h1 className="text-center mt-5"> Comments </h1>
-        <div className="p-5">
-          <Comment name="Edson Galan" position="Fullstack Developer" company="Alpha Team" img="user-00.png"
-            comment="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
-          <Comment name="Juan Pérez" position="Backend Developer" company="Amazon" img="user-00.png"
-            comment="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
-          <Comment name="Luis García" position="Frontend Developer" company="Uber" img="user-00.png"
-            comment="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="text-center">
+          <h1 className="text-center mt-5"> Comments </h1>
+          <div className="p-5">
+            <Comment name="Edson Galan" position="Fullstack Developer" company="Alpha Team" img="user-00.png"
+              comment="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+            <Comment name="Juan Pérez" position="Backend Developer" company="Amazon" img="user-00.png"
+              comment="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+            <Comment name="Luis García" position="Frontend Developer" company="Uber" img="user-00.png"
+              comment="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/comments/comments.jsx
+++ src/components/comments/comments.jsx
@@ -1,22 +1,26 @@
 import React from 'react'; // Esta línea es opcional
 import './comments.css';
 
-function Comment(props) {
-  return (
-    <div className="card mb-3">
-      <div className="card-body p-0">
-        <div className="row">
-          <div className="col-md-5">
-            <img className="img-fluid comment-img" src={ require(`../../img/${ props.img }`) } alt={ props.name } />
-          </div>
-          <div className="col-md-7 text-content">
-            <p className="comment-user-name"> { props.name } </p>
-            <p className="comment-position"> { props.position } in <strong> { props.company } </strong> </p>
-            <p className="comment"> "{ props.comment }" </p>
+class Comment extends React.Component {
+
+  render() {
+    return (
+      <div className="card mb-3">
+        <div className="card-body p-0">
+          <div className="row">
+            <div className="col-md-5">
+              <img className="img-fluid comment-img" src={ require(`../../img/${ this.props.img }`) } alt={ this.props.name } />
+            </div>
+            <div className="col-md-7 text-content">
+              <p className="comment-user-name"> { this.props.name } </p>
+              <p className="comment-position"> { this.props.position } in <strong> { this.props.company } </strong> </p>
+              <p className="comment"> "{ this.props.comment }" </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 export default Comment;
